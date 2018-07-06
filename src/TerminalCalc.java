7e6999637804e6b7df341dc4a8e873eb936eb2c4
Danny Araujo <dannyara@buffalo.edu>
2018-07-05 23:26:17 -0400

    Refactored variables and change from int to Double
    - Created methods to handle string/equation building, input recieving, overall flow of the program
    
    -fixed error and exception handling
    
    - added funcitonality for more complex arithmitic variables (can add decimals using Double variables)
    
    - overall code and performance optimization

--- src/TerminalCalc.java
+++ src/TerminalCalc.java
@@ -4,74 +4,53 @@ import java.util.Scanner;
 
 public class TerminalCalc{
 public static char operation;
-public static int val1, val2;
+public static double val1, val2;
 public static double ans;
-	public static void main(String[] args) {
-		
+public static Scanner keyboardInput;
+public static void main(String[] args) {
 		boolean resetCalc = true;
+		
 		System.out.println("Welcome to Terminal Calculator!");
 		
 		while(resetCalc == true){
-
-			Scanner keyboardInput = new Scanner(System.in);
-			try{
-				getInput(keyboardInput);
-			}catch(Exception e) {
+			boolean isValidEquation = true;
+			keyboardInput = new Scanner(System.in);
+			try{ 
+				getInput(keyboardInput); 
+			}
+			catch(Exception e) {
 				System.out.println("error!!1 \n Try Again");
-				keyboardInput.reset();
-				getInput(keyboardInput);
+				System.out.println(e);
+				isValidEquation = false;
 			}
-			ans = getAnswer(operation);
-			String equation = val1 + " " + operation + " "+ val2 + " = "; 
-			if(ans == (int)ans) 
-				equation += (int)ans;
-			else equation += ans;
 			
-			System.out.println(equation);
-			
-			
-		Scanner reset =new Scanner(System.in);
-		
-		System.out.println("reset? y/n ");
-		String toContinue = reset.next();
-		try{
-			if(toContinue.equals("y")) resetCalc=true;
-			else{ 
-				reset.close();
-				keyboardInput.close();
-				resetCalc = false;
-				
-				System.exit(-1);
-				
+			if(isValidEquation) {
+				ans = getAnswer(operation);
+				System.out.println(toString(val1, val2, operation));
+				reset(resetCalc);
 			}
-		}catch (Exception e){
-			
-			System.out.println("didn't choose anything");
-			reset.close();
-		}
 		
 		}
 	}
 	
-	public static int mult(int x, int y){
+	public static double mult(double x, double y){
 		return x*y;
 	}
-	public static double div(int x, int y){
+	public static double div(double x, double y){
 		return ((double)x/y);	
 	}
 	
 	static  void getInput(Scanner sc) {
 		System.out.println("Enter equation below (use spaces)");
-	//	try{
+
+		//if(val1 == (int)val1) val1 = sc.nextInt();
+		 val1 = sc.nextDouble();
 		
-			val1 = sc.nextInt();
-			operation = sc.next().charAt(0);
-			val2 = sc.nextInt();
+		operation = sc.next().charAt(0);
 			
-		//} catch (Exception e){
-		//	System.out.println("error, try again \r Exception thrown was: " + e);
+		//if(val2 == (int)val2)	val2 = sc.nextInt();
+		 val2 = sc.nextDouble();
 			
-	//	}
 		
 	 }
 	 
@@ -79,11 +58,11 @@ public static double ans;
 		 switch (op){
 			
 			case '+':
-				return (double)val1+val2;
+				return val1+val2;
 				
 			
 			case '-':
-				return (double)(val1-val2);
+				return (val1-val2);
 				
 			
 			case '*':
@@ -91,8 +70,8 @@ public static double ans;
 				
 			
 			case '/':
-				if(val1%val2==0) return (double)(val1/val2);
-				else return (double)(div(val1,val2));
+				if(val1%val2==0) return (val1/val2);
+				else return (div(val1,val2));
 			
 			case '^':
 				return(double)(Math.pow(val1, val2));
@@ -104,7 +83,42 @@ public static double ans;
 		}
 		
 	 }
-
+	 static StringBuilder toString(double v1, double v2, char op) {
+		 StringBuilder equation = new StringBuilder(); 
+		 
+		 if(v1 == (int)v1) equation.append((int)v1);
+		 else equation.append(v1);
+		
+		 equation.append(" "+ op + " " );
+		 
+		 if(v2 == (int)v2) equation.append((int)v2);
+		 else equation.append(v2);
+		 
+		 equation.append(" = ");
+		 
+		 if(ans == (int)ans) 
+				equation.append((int)ans);
+		 else equation.append( ans);
+			
+		 return equation;
+	 }
+	 static void reset(boolean resetCalc) {
+	 System.out.println("reset? y/n ");	
+		Scanner reset = new Scanner(System.in);
+		String toContinue = reset.next();
+		
+			if(toContinue.equals("y")) resetCalc=true;
+			else if(toContinue.equals("n")){ 
+				reset.close();
+				keyboardInput.close();
+				resetCalc = false;
+				System.exit(0);
+			}
+			else {
+			System.out.println("didn't catch that, try again");
+			reset(resetCalc);
+		}
+	 }
 
 }
 
